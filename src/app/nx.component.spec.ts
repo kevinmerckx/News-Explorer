d1bc25cb4e665b978e99ab87b1015b299d7d8fae
Michael Mannseicher <m.mansi1@gmx.net>
2018-05-15 17:33:11 +0200

    - added primeng dataTables with an small news mock - deleted testing stuff

--- src/app/nx.component.spec.ts
+++ src/app/nx.component.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
+import { NxComponent } from './nx.component';
+describe('NxComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        NxComponent
       ],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(NxComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
---
+++ src/app/nx.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'nx-root',
+  templateUrl: './nx.component.html',
+  styleUrls: ['./nx.component.sass']
+})
+export class NxComponent {
+}
